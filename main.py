55371c5037435e96ea1e13ab0901d213dcae6342
Dellon Zerus <dellonzerus@gmail.com>
2020-04-18 11:53:32 -0700

    Implemented basic file listing functionality
    Why:
    Groundwork for future operations
    
    How:
    Using glob to get files, added recursive and directory arguments to
    determine how and where to get the files from.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import glob
 import argparse
 
 IS_DEBUG = False
@@ -11,18 +12,40 @@ def main():
     if args.version:
         print (PROG_VERSION)
         return
+
     if args.num_parts:
         num_parts = args.num_parts
     else:
         args.num_parts = 4
+
+    if not args.directory:
+        args.directory = os.getcwd
+
     if args.is_debug:
         IS_DEBUG = True
+
     dbg_print("Successfully parsed args")
+
+    mergeAudio(args)
 
 
 def dbg_print(*args, **kwargs):
     if IS_DEBUG:
         print(*args, **kwargs)
+
+
+def mergeAudio(args):
+    workingDir = os.getcwd()
+    if args.recursive:
+        workingFiles = os.path.join(workingDir, "**/*.mp3")
+    else:
+        workingFiles = os.path.join(workingDir, "*.mp3")
+
+    dbg_print(f"Started script at path {workingDir}")
+    dbg_print(f"Wildcard pattern is {workingFiles}")
+
+    files = glob.glob(workingFiles)
+    print(files)
 
 
 # Parses command line args passed to the program
@@ -34,7 +57,8 @@ def parseArgs():
     help_texts = {
         "version": "Output program version and exit.",
         "num_parts": "Specify number of parts you want to split into. Default: 4",
-        "debug"  : "Start in debug mode."
+        "recursive": "Specify that subdirs should be processed.",
+        "debug"  : "Start in debug mode. Prints text to follow program's flow."
     }
 
     parser.add_argument("-v", "--version",
@@ -46,6 +70,16 @@ def parseArgs():
                         action="store",
                         dest="num_parts",
                         help=help_texts["num_parts"])
+
+    parser.add_argument("-d",
+                        action="store",
+                        dest="directory",
+                        help=help_texts["directory"])
+
+    parser.add_argument("-r",
+                        action="store_true",
+                        dest="recursive",
+                        help=help_texts["recursive"])
 
     parser.add_argument("--dbg",
                         action="store_true",
